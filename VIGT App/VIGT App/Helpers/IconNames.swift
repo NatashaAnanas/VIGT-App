9a7802aee421bb13ea34201f32c16fe487294049
Natasha Ananas <konnat7@mail.ru>
2024-11-22 02:05:14 +0300

    Изменила стартовый фон

---
+++ VIGT App/VIGT App/Helpers/IconNames.swift
@@ -0,0 +1,12 @@
+//
+//  IconNames.swift
+//  VIGT App
+//
+//  Created by Наталья Коновалова on 22.11.2024.
+//
+
+import Foundation
+
+public enum IconNames {
+    static let mainBackgroundImage = "mainBackgroundImage"
+}
