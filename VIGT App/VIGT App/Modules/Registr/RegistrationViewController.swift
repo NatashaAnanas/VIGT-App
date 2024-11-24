83aa958a23d18b03cbd6283dd83e3ff819088a53
Natasha Ananas <konnat7@mail.ru>
2024-11-24 15:48:22 +0300

    [VIGT-4.1] - правки

--- VIGT App/VIGT App/Modules/Registr/RegistrationViewController.swift
+++ VIGT App/VIGT App/Modules/Registr/RegistrationViewController.swift
@@ -7,12 +7,12 @@
 
 import UIKit
 
-final class RegistrationViewController: UIViewController {
+final class RegistrationViewController1: UIViewController {
     
     let emailTextField: CustomTextField = {
         let textField = CustomTextField()
         textField.font = .systemFont(ofSize: 16)
-        textField.placeholder = "e-mail"
+        textField.placeholder = Localization.email.rawValue
         textField.backgroundColor = .init(red: 1, green: 1, blue: 1, alpha: 0.5)
         textField.layer.cornerRadius = 8
         textField.tintColor = .darkGray
@@ -22,24 +22,35 @@ final class RegistrationViewController: UIViewController {
     
     let passwordTextField: CustomTextField = {
         let textField = CustomTextField()
-        textField.font = .systemFont(ofSize: 16)
-        textField.placeholder = "Пароль"
+        textField.placeholder = Localization.password.rawValue
         textField.backgroundColor = .init(red: 1, green: 1, blue: 1, alpha: 0.5)
         textField.layer.cornerRadius = 8
         textField.tintColor = .darkGray
         textField.textPadding = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 0)
+        textField.isSecureTextEntry = true
         textField.translatesAutoresizingMaskIntoConstraints = false
         return textField
     }()
     
     let registerButton: UIButton = {
         let button = UIButton()
-        button.setTitle("Зарегистрироваться", for: .normal)
+        button.setTitle(Localization.registration.rawValue, for: .normal)
         button.backgroundColor = .none
         button.titleLabel?.textColor = .darkGray
         button.titleLabel?.font = .systemFont(ofSize: 18, weight: .bold)
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
+    }()
+    
+    let errorLabel: UILabel = {
+        let label = UILabel()
+        label.text = Localization.emailIsAlreadyInUse.rawValue
+        label.textColor = .red
+        label.isHidden = true
+        label.contentMode = .center
+        label.font = .systemFont(ofSize: 18, weight: .bold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
     }()
     
     override func viewDidLoad() {
@@ -50,7 +61,9 @@ final class RegistrationViewController: UIViewController {
     private func setupUI() {
         view.addSubviews(emailTextField,
                          passwordTextField,
-                         registerButton)
+                         registerButton,
+                         errorLabel
+        )
         
         view.backgroundColor = .init(red: 0.1, green: 0.1, blue: 0.1, alpha: 0.9)
         
@@ -64,13 +77,32 @@ final class RegistrationViewController: UIViewController {
                                  for: .touchUpInside)
     }
     
+    private func writeUserData() {
+        let email = emailTextField.text ?? ""
+        let password = passwordTextField.text  ?? ""
+        
+        if (!email.isEmpty && !password.isEmpty) {
+            if UserDefaults.standard.object(forKey: email) == nil {
+                errorLabel.isHidden = true
+                UserDefaults.standard.set(password, forKey: email)
+                dismiss(animated: true)
+            } else {
+                errorLabel.text = Localization.emailIsAlreadyInUse.rawValue
+                errorLabel.isHidden = false
+            }
+        } else {
+            errorLabel.text = Localization.fillInAllFields.rawValue
+            errorLabel.isHidden = false
+        }
+    }
+    
     @objc private func registrationButtonAction() {
-        dismiss(animated: true)
+        writeUserData()
         
     }
 }
 
-extension RegistrationViewController {
+extension RegistrationViewController1 {
     
     private func setupConstraints() {
         NSLayoutConstraint.activate([
@@ -92,7 +124,11 @@ extension RegistrationViewController {
             registerButton.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor, constant: .spacing8),
             registerButton.heightAnchor.constraint(equalToConstant: .spacing32)
         ])
+        
+        NSLayoutConstraint.activate([
+            errorLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            errorLabel.bottomAnchor.constraint(equalTo: emailTextField.topAnchor, constant: -.spacing16),
+            errorLabel.heightAnchor.constraint(equalToConstant: .spacing32)
+        ])
     }
 }
-
-
