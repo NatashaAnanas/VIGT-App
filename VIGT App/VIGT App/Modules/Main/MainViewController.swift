7fb95c7cb1cfcab2d3f74e031133aa01fe85fdc5
Natasha Ananas <konnat7@mail.ru>
2024-11-26 15:21:04 +0300

    [VIGT-10] - добавила таблицу

--- VIGT App/VIGT App/Modules/Main/MainViewController.swift
+++ VIGT App/VIGT App/Modules/Main/MainViewController.swift
@@ -9,8 +9,79 @@ import UIKit
 
 final class MainViewController: UIViewController {
     
+    private let mainTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .yellow
+        mainTableView.delegate = self
+        mainTableView.dataSource = self
+        
+        view.addSubview(mainTableView)
+        setupConstraints()
+    }
+}
+
+// MARK: - Constraints
+
+extension MainViewController {
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            mainTableView.topAnchor.constraint(equalTo: view.topAnchor),
+            mainTableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            mainTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            mainTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+        ])
+    }
+}
+
+// MARK: - UITableViewDataSource
+
+extension MainViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        15
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "\(indexPath.row)"
+        return cell
+    }
+}
+
+// MARK: - UITableViewDelegate
+
+extension MainViewController: UITableViewDelegate {
+    
+}
+
+// TODO: - вынести в отдельный файл
+extension MainViewController {
+    private func setupInitialStateNotificationCenter() {
+        NotificationCenter.default
+            .addObserver(self,
+                         selector: #selector(keyboardWillShow),
+                         name: UIResponder.keyboardWillShowNotification,
+                         object: nil)
+        NotificationCenter.default
+            .addObserver(self,
+                         selector: #selector(keyboardWillHide),
+                         name: UIResponder.keyboardWillHideNotification,
+                         object: nil)
+    }
+    
+    @objc private func keyboardWillShow(notification: Notification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            mainTableView.contentInset.bottom = keyboardSize.height
+        }
+    }
+    
+    @objc private func keyboardWillHide(notification: Notification) {
+        mainTableView.contentInset.bottom = 0
     }
 }
